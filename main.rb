7892d63c29bf9b4b4dd2a7b05c0a99009d54dfc1
Matias Jara <mjara.figueroa@gmail.com>
2017-08-16 11:52:05 -0300

    ingresando metodos y main terminados

--- main.rb
+++ main.rb
@@ -3,12 +3,48 @@ require_relative 'metodos.rb'
 
 # class alumnos
 class Alumnos
-  attr_accessor :nombre, :nota1, :nota2, :nota3, :inasistencias
+  attr_accessor :nombre, :inasistencias
   def initialize(nombre, nota1, nota2, nota3, inasistencias)
     @nombre = nombre
-    @nota1 = nota1
-    @nota2 = nota2
-    @nota3 = nota3
+    @nota1 = nota1.to_i
+    @nota2 = nota2.to_i
+    @nota3 = nota3.to_i
     @inasistencias = inasistencias
   end
+
+  # metodo retorna promedio de notas
+  def promedios
+    promedio = (@nota1.to_f + @nota2.to_f + @nota3.to_f) / 3
+    promedio
+  end
+end
+
+alumnos = asignador
+
+puts "Ingrese una opcion:\n 1)Salir\n 2)Cantidad de alumnos\n 3)Mostrar alumno y promedio\n 4)crear archivo promedios\n 5)mostrar alumnos sobre 7"
+op = gets.chomp.to_i
+
+loop do
+  case op
+  when 1
+    puts 'Adios!!'
+    break
+  when 2
+    contador = contar_lineas
+    puts "hay un total de #{contador} alumnos"
+  when 3
+    alumnos.each do |alumno|
+      promedio = alumno.promedios
+      puts "#{alumno.nombre} tiene promedio #{promedio}"
+    end
+  when 4
+    puts write_promedios(alumnos)
+  when 5
+    puts aprobados(alumnos)
+  else
+    puts "Error: Opcion invalida!!\nIngrese una opcion valida"
+  end
+
+  puts "\nIngrese una opcion:\n 1)Salir\n 2)Cantidad de alumnos\n 3)Mostrar alumno y promedio\n 4)crear archivo promedios\n 5)mostrar alumnos sobre 7"
+  op = gets.chomp.to_i
 end
---
+++ metodos.rb
@@ -0,0 +1,47 @@
+# metodo read
+def read_alumnos
+  file = File.open('alumnos.csv', 'r')
+  content = file.readlines
+  file.close
+  line = content.map { |e| e.delete(",\n") }
+  line
+end
+
+def write_promedios(arr = [])
+  file = File.open('promedios.txt', 'w')
+  arr.each do |alumno|
+    file.puts "#{alumno.nombre}, #{alumno.promedios}, #{alumno.inasistencias} inasistencias"
+  end
+  file.close
+  'Archivo creado con exito!'
+end
+
+def asignador
+  alum = []
+  line = read_alumnos
+  line.each do |alumno|
+    contador = 0
+    arr = alumno.split(' ')
+    arr.each do |x|
+      contador += 1 if x == 'A'
+    end
+    alum << Alumnos.new(arr[0], arr[1], arr[2], arr[3], contador)
+  end
+  alum
+end
+
+def contar_lineas
+  line = read_alumnos
+  contador = line.count
+  contador
+end
+
+def aprobados(arr = [])
+  aprobados = ''
+  arr.each do |alumno|
+    if alumno.promedios > 7
+      aprobados += "#{alumno.nombre} aprueba con nota #{alumno.promedios}\n"
+    end
+  end
+  aprobados
+end
